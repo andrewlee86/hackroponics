4e51848a7d2f4ea75afcf211f9b2f156e0ba6452
Andrew Lee <andrewlee86@gmail.com>
2013-12-08 14:47:54 +0800

    Merged webserver with main code

--- src/WebServer/WebServer.ino
+++ src/WebServer/WebServer.ino
@@ -18,8 +18,7 @@
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,0,100);
 
 // Initialize the Ethernet server library
@@ -28,12 +27,11 @@ IPAddress ip(192,168,0,100);
 EthernetServer server(80);
 
 void setup() {
- // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-
 
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
@@ -62,7 +60,7 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-    client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
